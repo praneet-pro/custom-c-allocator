88fbc391e054ea106523bed1cc7db6071978b3f6
Praneet S <spraneet333@gmail.com>
2026-07-09 15:36:04 +0000

    feat: added the first-fit search algorithm

--- free_list.c
+++ free_list.c
@@ -7,3 +7,17 @@ typedef struct Block {
     struct Block* next;
     struct Block* prev;
 } Block;
+
+Block* free_list_head = NULL;
+
+Block* find_free_block(size_t required_size) {
+    Block* curr = free_list_head;
+
+    while(curr != NULL) {
+        if(curr->size >= required_size) 
+            return curr;
+
+        curr = curr->next;
+    }
+    return NULL;
+}
