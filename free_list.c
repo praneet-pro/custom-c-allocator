8942c9bab8cbefb75599718ac68651c2e14d003f
Praneet S <spraneet333@gmail.com>
2026-07-10 15:25:36 +0000

    feat: implemented block splitting logic for memory allocation

--- free_list.c
+++ free_list.c
@@ -10,6 +10,7 @@ typedef struct Block {
 
 Block* free_list_head = NULL;
 
+
 Block* find_free_block(size_t required_size) {
     Block* curr = free_list_head;
 
@@ -20,4 +21,29 @@ Block* find_free_block(size_t required_size) {
         curr = curr->next;
     }
     return NULL;
+}
+
+
+void split_block(Block* total_block, size_t required_size) {
+    size_t remaining_size = total_block->size - required_size - sizeof(Block);
+
+    if(remaining_size <= sizeof(Block)) {
+        return;
+    }
+
+    Block* new_block = (Block*)((char*)total_block + sizeof(Block) + required_size);
+
+    new_block->size = remaining_size;
+    new_block->is_free = 1;
+
+    new_block->next = total_block->next;
+    new_block->prev = total_block;
+
+    if(total_block->next != NULL) {
+        total_block->next->prev = new_block;
+    }
+
+    total_block->next = new_block;
+    total_block->size = required_size;
+    total_block->is_free = 0;
 }
