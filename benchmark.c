ba516b50cab920792152bcb400a0991becd57354
Praneet S <spraneet333@gmail.com>
2026-08-12 15:28:46 +0000

    feat: finalize segregated list memory allocator
    Implemented 12-bin segregated routing, boundary-tag coalescing, and benchmarked amortized O(1) small-bin performance against standard malloc.

--- benchmark.c
+++ benchmark.c
@@ -1,82 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "free_list.h" 
 
-#define ITERATIONS 1000000
-#define ARRAY_SIZE 256
-#define MAX_ALLOC_SIZE 1024
+extern void* my_malloc(size_t size);
+extern void my_free(void* ptr);
+
+#define ITERATIONS 500
+#define MAX_ALLOC_SIZE 4096
+#define MIN_ALLOC_SIZE 1
 
 int main() {
+    void* pointers_sys[ITERATIONS];
+    void* pointers_custom[ITERATIONS];
+    size_t random_sizes[ITERATIONS];
     clock_t start, end;
     double cpu_time_used;
 
-    void* standard_blocks[ARRAY_SIZE] = {NULL};
-    void* custom_blocks[ARRAY_SIZE] = {NULL};
-
+    // Pre-generate random sizes to hit EVERY bin in your allocator
     srand(time(NULL));
-
-    // 1. STANDARD MALLOC CHAOS TEST
-    start = clock();
-    
     for (int i = 0; i < ITERATIONS; i++) {
-        int index = rand() % ARRAY_SIZE;
-        
-        if (standard_blocks[index] != NULL) {
-            // Slot is full: Free it to create fragmentation
-            free(standard_blocks[index]);
-            standard_blocks[index] = NULL;
-        } else {
-            // Slot is empty: Allocate a random size
-            size_t size = (rand() % MAX_ALLOC_SIZE) + 1;
-            standard_blocks[index] = malloc(size);
-            
-            // Anti-lazy OS trick
-            if (standard_blocks[index] != NULL) {
-                ((char*)standard_blocks[index])[0] = 'A';
-            }
-        }
+        random_sizes[i] = (rand() % (MAX_ALLOC_SIZE - MIN_ALLOC_SIZE + 1)) + MIN_ALLOC_SIZE;
     }
-    
-    // Clean up any blocks left in the array at the end
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        if (standard_blocks[i] != NULL) {
-            free(standard_blocks[i]);
-        }
-    }
-    
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Standard malloc chaos time: %f seconds\n", cpu_time_used);
 
-    // 2. CUSTOM MALLOC CHAOS TEST
+    printf("Running Full-Spectrum Chaos Benchmark (1 to 4096 bytes)...\n");
+
+    // --- SYSTEM MALLOC ---
     start = clock();
-    
     for (int i = 0; i < ITERATIONS; i++) {
-        int index = rand() % ARRAY_SIZE;
-        
-        if (custom_blocks[index] != NULL) {
-            my_free(custom_blocks[index]);
-            custom_blocks[index] = NULL;
-        } else {
-            size_t size = (rand() % MAX_ALLOC_SIZE) + 1;
-            custom_blocks[index] = my_malloc(size);
-            
-            if (custom_blocks[index] != NULL) {
-                ((char*)custom_blocks[index])[0] = 'A';
-            }
-        }
+        pointers_sys[i] = malloc(random_sizes[i]);
     }
-    
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        if (custom_blocks[i] != NULL) {
-            my_free(custom_blocks[i]);
-        }
+    for (int i = 0; i < ITERATIONS; i++) {
+        free(pointers_sys[i]);
     }
-    
     end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Custom malloc chaos time: %f seconds\n", cpu_time_used);
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Standard malloc time: %f seconds\n", cpu_time_used);
+
+    // --- CUSTOM MALLOC ---
+    start = clock();
+    for (int i = 0; i < ITERATIONS; i++) {
+        pointers_custom[i] = my_malloc(random_sizes[i]);
+    }
+    for (int i = 0; i < ITERATIONS; i++) {
+        my_free(pointers_custom[i]);
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Custom malloc time:   %f seconds\n", cpu_time_used);
 
     return 0;
 }
--- free_list.c
+++ free_list.c
@@ -17,22 +17,83 @@ typedef struct Block {
 #define SET_ALLOC(block) ((block)->size &= ~1)
 #define SET_SIZE_AND_FLAG(block, new_size, free_flag) ((block)->size = (new_size) | (free_flag))
 
-static Block* free_list_head = NULL;
+static Block* segregated_lists[12] = {NULL};
 static void* heap_start = NULL;
+
+// To get index of the suitable bin in segregated list
+int get_bin_index(size_t size) {
+    if(size <= 128) {
+        return (size >> 4) - 1;
+    }
+
+    else if(size <= 256) {
+        return 8;
+    }
+    else if(size <= 512) {
+        return 9;
+    }
+    else if(size <= 1024) {
+        return 10;
+    }
+
+    else {
+        return 11;
+    }
+}
 
 // Searches for free block 
 Block* find_free_block(size_t required_size) {
-    Block* curr = free_list_head;
+    int starting_index = get_bin_index(required_size);
 
-    while(curr != NULL) {
-        if(GET_SIZE(curr) >= required_size) 
-            return curr;
+    for(int i = starting_index; i < 12; i++) {
+        Block* curr = segregated_lists[i];
 
-        curr = curr->next;
+        while(curr != NULL) {
+            if(GET_SIZE(curr) >= required_size) {
+                return curr;
+            }
+            curr = curr->next;
+        }
     }
+
     return NULL;
 }
 
+// Inserts a block into the segregated list
+void insert_block(Block* block) {
+    size_t size = GET_SIZE(block);
+
+    int index = get_bin_index(size);
+
+    block->next = segregated_lists[index];
+    block->prev = NULL;
+
+    if(segregated_lists[index] != NULL) {
+        segregated_lists[index]->prev = block;
+    }
+
+    segregated_lists[index] = block;
+}
+
+
+// Removes a block from segregated list
+void remove_block(Block* block) {
+    int size = GET_SIZE(block);
+    int index = get_bin_index(size);
+
+    if(block->prev != NULL) {
+        block->prev->next = block->next;
+    } else {
+        segregated_lists[index] = block->next;
+    }
+
+    if(block->next != NULL) {
+        block->next->prev = block->prev;
+    }
+
+    block->next = NULL;
+    block->prev = NULL;
+}
 
 // Splits the free block to prevent wasteage of memory
 // Gives the required memory size
@@ -41,16 +102,6 @@ void split_block(Block* total_block, size_t required_size) {
 
     // CASE 1: No split, Removing the block from free list
     if(total_size <= required_size + sizeof(Block) + sizeof(Block*)) {
-        if(total_block->prev != NULL) {
-            total_block->prev->next = total_block->next;
-        } else {
-            free_list_head = total_block->next;
-        }
-
-        if(total_block->next != NULL) {
-            total_block->next->prev = total_block->prev;
-        } 
-
         SET_ALLOC(total_block);
         return;
     }
@@ -68,20 +119,9 @@ void split_block(Block* total_block, size_t required_size) {
     Block** new_footer = (Block**)((char*)new_block + sizeof(Block) + remaining_size);
     *new_footer = new_block;
 
-    new_block->next = total_block->next;
-    new_block->prev = total_block->prev;
-
-    if(new_block->prev != NULL) {
-        new_block->prev->next = new_block;
-    } else {
-        free_list_head = new_block;
-    }
-
-    if(new_block->next != NULL) {
-        new_block->next->prev = new_block;
-    }
-
     SET_SIZE_AND_FLAG(total_block, required_size, 0);
+
+    insert_block(new_block);
 }
 
 // Stiches the physically neighbouring free blocks together
@@ -92,20 +132,12 @@ void coalesce_blocks(Block* curr) {
 
     if ((char*)physical_right < ((char*)heap_start + HEAP_SIZE)) {
         if(IS_FREE(physical_right)) {
-             size_t right_size = GET_SIZE(physical_right);
+            remove_block(physical_right);
 
+            size_t right_size = GET_SIZE(physical_right);
             size_t new_size = curr_size + sizeof(Block) + right_size + sizeof(Block*);
-            SET_SIZE_AND_FLAG(curr, new_size, 1);
 
-            if(physical_right->prev != NULL) {
-                physical_right->prev->next = physical_right->next;
-            }
-            else {
-                free_list_head = physical_right->next;
-            }
-            if(physical_right->next != NULL) {
-                physical_right->next->prev = physical_right->prev;
-            }
+            SET_SIZE_AND_FLAG(curr, new_size, 1);
 
             Block** right_footer = (Block**)((char*)curr + sizeof(Block) + new_size);
             *right_footer = curr;
@@ -120,27 +152,21 @@ void coalesce_blocks(Block* curr) {
         Block* physical_left = *left_footer;
 
         if(IS_FREE(physical_left)) {
-            size_t left_size = GET_SIZE(physical_left);
+            remove_block(physical_left);
 
+            size_t left_size = GET_SIZE(physical_left);
             size_t new_size = left_size + sizeof(Block) + sizeof(Block*) + curr_size;
+
             SET_SIZE_AND_FLAG(physical_left, new_size, 1);
 
             Block** new_footer = (Block**)((char*)physical_left + sizeof(Block) + new_size);
             *new_footer = physical_left;
 
-            if(curr->prev != NULL) {
-                curr->prev->next = curr->next;
-            }
-            else {
-                free_list_head = curr->next;
-            }
-            if(curr->next != NULL) {
-                curr->next->prev = curr->prev;
-            }
-
             curr = physical_left;
         }
     }
+
+    insert_block(curr);
 }
 
 // frees the memory passed by the user(The user doesnot pass the header so we navigate to it)
@@ -150,21 +176,12 @@ void my_free(void *ptr) {
     Block* curr = (Block*)((char*)ptr - sizeof(Block));
     SET_FREE(curr);
 
-    curr->next = free_list_head;
-    curr->prev = NULL;
-
-    if(free_list_head != NULL) {
-        free_list_head->prev = curr;
-    }
-
-    free_list_head = curr;
-
     coalesce_blocks(curr);
 }
 
 // Gets 1MB of memory from OS and links to head for further usage
 void init_heap() {
-    if(free_list_head != NULL) return;
+    if(heap_start != NULL) return;
 
     void* raw_memory = mmap(NULL, HEAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
@@ -173,37 +190,36 @@ void init_heap() {
         return;
     }
 
-    free_list_head = (Block*)raw_memory;
-
     heap_start = raw_memory;
 
+    Block* initial_block = (Block*)raw_memory;
     size_t initial_size = HEAP_SIZE - sizeof(Block) - sizeof(Block*);
-    SET_SIZE_AND_FLAG(free_list_head, initial_size, 1);
 
-    free_list_head->next = NULL;
-    free_list_head->prev = NULL;
+    SET_SIZE_AND_FLAG(initial_block, initial_size, 1);
 
-    Block** footer = (Block**)((char*)free_list_head + sizeof(Block) + initial_size);
-    *footer = free_list_head;
+    Block** footer = (Block**)((char*)initial_block + sizeof(Block) + initial_size);
+    *footer = initial_block;
+
+    insert_block(initial_block);
 }
 
 // Gets the memory to the user
 void* my_malloc(size_t size) {
     if(size == 0) return NULL;
 
-    if(free_list_head == NULL) {
+    if(heap_start == NULL) {
         init_heap();
     }
 
     size_t alligned_size = (size + 15) & ~15;
 
     Block* found_block = find_free_block(alligned_size);
-
     if(found_block == NULL) {
         printf("Error: Out of memory.\n");
         return NULL;
     }
 
+    remove_block(found_block);
     split_block(found_block, alligned_size);
 
     return (void*)((char*)found_block + sizeof(Block));
